de60fe354984faabda5de7db142fb5d44d16cfb8
Jinyi Xiang <grantsjtu@sjtu.edu.cn>
2020-02-16 20:00:25 +0800

    First Commit

---
+++ LearnGit/LearnGit/main.cpp
@@ -0,0 +1,16 @@
+//
+//  main.cpp
+//  LearnGit
+//
+//  Created by 向谨溢 on 2020/2/16.
+//  Copyright © 2020 向谨溢. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "this project is a learning project";
+    std::cout << "Hello, World!\n";
+    return 0;
+}
